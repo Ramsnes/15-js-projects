a8552646f0856185ec697ae4ad02d3552bf5c35d
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-08-30 15:25:54 +0200

    completed project 1b

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,17 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]; //array
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]; //array of color selections
+const btn = document.getElementById("btn"); // button
+const color = document.querySelector(".color"); // color descriptor
 
-// This is the button-click function  // Change to arrow function later
+// button-click function goal: get random number between 0 - 3 (colors array line 1)
 btn.addEventListener("click", function () {
-  // goal: get random number between 0 - 3 (colors array line 1)
-  const randomNumber = getRandomNumber(); // hardcodes the third property in colors array
+  const randomNumber = getRandomNumber();
   console.log(randomNumber);
 
-  document.body.style.backgroundColor = colors[randomNumber]; // changes bgc to the function getRandomNumber()
+  document.body.style.backgroundColor = colors[randomNumber]; // bgc becomes getRandomNumber()
   color.textContent = colors[randomNumber]; // .textContent displays color being used colors[2]
 });
 
+// returns a random number between 0 - 4 (length of const colors)
 const getRandomNumber = () => {
   return Math.floor(Math.random() * colors.length);
 };
